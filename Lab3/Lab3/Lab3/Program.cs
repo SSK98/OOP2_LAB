a2fe5c217d610af78b8b830d1d6e5df932bc87c2
Sadman <ssakibk@gmail.com>
2020-10-31 11:43:56 +0600

    Lab Task 3

---
+++ Lab3/Lab3/Lab3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labTask3.student
+{
+    internal class Student
+    {
+        string name;
+        string id;
+        string department;
+        float cgpa;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Id
+        {
+            get { return id; }
+            set { name = value; }
+        }
+        public string Department
+        {
+            get { return department; }
+            set { name = value; }
+        }
+        public float Cgpa
+        {
+            get { return cgpa; }
+            set { name = value; }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("id : " + id);
+            Console.WriteLine("Department : " + department);
+            Console.WriteLine("CGPA : " + cgpa);
+        }
+    }
+}
