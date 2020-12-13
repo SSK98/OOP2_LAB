d303bf51b5d965fb4e54823078e243ebfa0a55d4
Sadman <ssakibk@gmail.com>
2020-12-14 00:02:25 +0600

    Fixed an error

--- Lab_8/Lab_Performance/Lab_Performance/Form1.Designer.cs
+++ Lab_8/Lab_Performance/Lab_Performance/Form1.Designer.cs
@@ -30,18 +30,18 @@ namespace Lab_Performance
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.LabelUserName = new System.Windows.Forms.Label();
-            this.textBoxUserName = new System.Windows.Forms.TextBox();
-            this.textBoxPassword = new System.Windows.Forms.TextBox();
-            this.labelPassword = new System.Windows.Forms.Label();
             this.buttonLogin = new System.Windows.Forms.Button();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.LabelUserName = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.MaskedTextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.buttonLogin);
             this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.buttonLogin);
             this.panel1.Controls.Add(this.labelPassword);
             this.panel1.Controls.Add(this.textBoxUserName);
             this.panel1.Controls.Add(this.LabelUserName);
@@ -50,33 +50,16 @@ namespace Lab_Performance
             this.panel1.Size = new System.Drawing.Size(537, 468);
             this.panel1.TabIndex = 0;
             // 
-            // LabelUserName
+            // buttonLogin
             // 
-            this.LabelUserName.AutoSize = true;
-            this.LabelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelUserName.Location = new System.Drawing.Point(72, 51);
-            this.LabelUserName.Name = "LabelUserName";
-            this.LabelUserName.Size = new System.Drawing.Size(97, 20);
-            this.LabelUserName.TabIndex = 0;
-            this.LabelUserName.Text = "User Name :";
-            // 
-            // textBoxUserName
-            // 
-            this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUserName.Location = new System.Drawing.Point(210, 38);
-            this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(228, 38);
-            this.textBoxUserName.TabIndex = 1;
-            this.textBoxUserName.TextChanged += new System.EventHandler(this.textBoxUserName_TextChanged);
-            // 
-            // textBoxPassword
-            // 
-            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPassword.Location = new System.Drawing.Point(210, 86);
-            this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(228, 38);
-            this.textBoxPassword.TabIndex = 3;
-            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.buttonLogin.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonLogin.Location = new System.Drawing.Point(210, 168);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(109, 36);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.Clicked);
             // 
             // labelPassword
             // 
@@ -89,16 +72,31 @@ namespace Lab_Performance
             this.labelPassword.Text = "Password :";
             this.labelPassword.Click += new System.EventHandler(this.label1_Click);
             // 
-            // buttonLogin
+            // textBoxUserName
             // 
-            this.buttonLogin.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.buttonLogin.Location = new System.Drawing.Point(210, 168);
-            this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(109, 36);
-            this.buttonLogin.TabIndex = 4;
-            this.buttonLogin.Text = "Login";
-            this.buttonLogin.UseVisualStyleBackColor = false;
-            this.buttonLogin.Click += new System.EventHandler(this.Clicked);
+            this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUserName.Location = new System.Drawing.Point(210, 38);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(228, 38);
+            this.textBoxUserName.TabIndex = 1;
+            this.textBoxUserName.TextChanged += new System.EventHandler(this.textBoxUserName_TextChanged);
+            // 
+            // LabelUserName
+            // 
+            this.LabelUserName.AutoSize = true;
+            this.LabelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelUserName.Location = new System.Drawing.Point(72, 51);
+            this.LabelUserName.Name = "LabelUserName";
+            this.LabelUserName.Size = new System.Drawing.Size(97, 20);
+            this.LabelUserName.TabIndex = 0;
+            this.LabelUserName.Text = "User Name :";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(210, 99);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(228, 20);
+            this.textBoxPassword.TabIndex = 5;
             // 
             // Form1
             // 
@@ -119,9 +117,9 @@ namespace Lab_Performance
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBoxUserName;
         private System.Windows.Forms.Label LabelUserName;
-        private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.Label labelPassword;
         private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.MaskedTextBox textBoxPassword;
     }
 }
 
--- Lab_8/Lab_Performance/Lab_Performance/Information.Designer.cs
+++ Lab_8/Lab_Performance/Lab_Performance/Information.Designer.cs
@@ -47,10 +47,28 @@ namespace Lab_Performance
             this.textBoxRetypedEmail = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBoxPasswordReg = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelPasswordInfo = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.labelRetyperPassword = new System.Windows.Forms.Label();
+            this.labelSequrityQues = new System.Windows.Forms.Label();
+            this.comboBoxSequrityQues = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelSequrityAns = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxState = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelContactInfo = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxAddressInfo = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.buttonOptional = new System.Windows.Forms.Button();
+            this.comboBoxMobileInfo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBoxPassword
@@ -112,6 +130,7 @@ namespace Lab_Performance
             // comboBox1Month
             // 
             this.comboBox1Month.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1Month.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1Month.FormattingEnabled = true;
             this.comboBox1Month.Items.AddRange(new object[] {
             "January",
@@ -296,15 +315,15 @@ namespace Lab_Performance
             this.textBoxPasswordReg.Size = new System.Drawing.Size(286, 26);
             this.textBoxPasswordReg.TabIndex = 22;
             // 
-            // label5
+            // labelPasswordInfo
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(62, 455);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 20);
-            this.label5.TabIndex = 21;
-            this.label5.Text = "Password";
+            this.labelPasswordInfo.AutoSize = true;
+            this.labelPasswordInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPasswordInfo.Location = new System.Drawing.Point(62, 455);
+            this.labelPasswordInfo.Name = "labelPasswordInfo";
+            this.labelPasswordInfo.Size = new System.Drawing.Size(86, 20);
+            this.labelPasswordInfo.TabIndex = 21;
+            this.labelPasswordInfo.Text = "Password";
             // 
             // label6
             // 
@@ -335,16 +354,224 @@ namespace Lab_Performance
             this.labelRetyperPassword.TabIndex = 24;
             this.labelRetyperPassword.Text = "Re-Type Passwordl";
             // 
+            // labelSequrityQues
+            // 
+            this.labelSequrityQues.AutoSize = true;
+            this.labelSequrityQues.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSequrityQues.Location = new System.Drawing.Point(61, 562);
+            this.labelSequrityQues.Name = "labelSequrityQues";
+            this.labelSequrityQues.Size = new System.Drawing.Size(142, 18);
+            this.labelSequrityQues.TabIndex = 26;
+            this.labelSequrityQues.Text = "Sequrity Question";
+            // 
+            // comboBoxSequrityQues
+            // 
+            this.comboBoxSequrityQues.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBoxSequrityQues.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxSequrityQues.FormattingEnabled = true;
+            this.comboBoxSequrityQues.Items.AddRange(new object[] {
+            "What is your pet name?",
+            "What is the name of your 1st proggraming Language?",
+            "What is the name of the hospital you were born?"});
+            this.comboBoxSequrityQues.Location = new System.Drawing.Point(222, 559);
+            this.comboBoxSequrityQues.Name = "comboBoxSequrityQues";
+            this.comboBoxSequrityQues.Size = new System.Drawing.Size(286, 21);
+            this.comboBoxSequrityQues.TabIndex = 27;
+            this.comboBoxSequrityQues.Text = "Choose a Sequrity Question";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.Location = new System.Drawing.Point(222, 605);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(286, 26);
+            this.textBox2.TabIndex = 29;
+            // 
+            // labelSequrityAns
+            // 
+            this.labelSequrityAns.AutoSize = true;
+            this.labelSequrityAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSequrityAns.Location = new System.Drawing.Point(61, 609);
+            this.labelSequrityAns.Name = "labelSequrityAns";
+            this.labelSequrityAns.Size = new System.Drawing.Size(129, 18);
+            this.labelSequrityAns.TabIndex = 28;
+            this.labelSequrityAns.Text = "Sequrity Answer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(219, 634);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 15);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "(Minimum 8 Charachters 1 Number Case-Sensetive";
+            // 
+            // comboBoxState
+            // 
+            this.comboBoxState.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBoxState.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxState.FormattingEnabled = true;
+            this.comboBoxState.Items.AddRange(new object[] {
+            "Dhaka",
+            "Chittagong",
+            "Sylhet",
+            "Khulna",
+            "Rangpur",
+            "Rajshahi"});
+            this.comboBoxState.Location = new System.Drawing.Point(686, 180);
+            this.comboBoxState.Name = "comboBoxState";
+            this.comboBoxState.Size = new System.Drawing.Size(228, 21);
+            this.comboBoxState.TabIndex = 37;
+            this.comboBoxState.Text = "Choose a State";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(552, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 20);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "State";
+            // 
+            // labelContactInfo
+            // 
+            this.labelContactInfo.AutoSize = true;
+            this.labelContactInfo.Font = new System.Drawing.Font("Keep on Truckin", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContactInfo.Location = new System.Drawing.Point(552, 42);
+            this.labelContactInfo.Name = "labelContactInfo";
+            this.labelContactInfo.Size = new System.Drawing.Size(230, 22);
+            this.labelContactInfo.TabIndex = 35;
+            this.labelContactInfo.Text = "Contact Information";
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxCity.Location = new System.Drawing.Point(686, 129);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(228, 26);
+            this.textBoxCity.TabIndex = 34;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(552, 132);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 20);
+            this.label9.TabIndex = 33;
+            this.label9.Text = "City";
+            // 
+            // textBoxAddressInfo
+            // 
+            this.textBoxAddressInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxAddressInfo.Location = new System.Drawing.Point(686, 84);
+            this.textBoxAddressInfo.Name = "textBoxAddressInfo";
+            this.textBoxAddressInfo.Size = new System.Drawing.Size(228, 26);
+            this.textBoxAddressInfo.TabIndex = 32;
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddress.Location = new System.Drawing.Point(552, 87);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(75, 20);
+            this.labelAddress.TabIndex = 31;
+            this.labelAddress.Text = "Address";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(686, 225);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 26);
+            this.textBox3.TabIndex = 39;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(552, 228);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 20);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "Zip";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox4.Location = new System.Drawing.Point(686, 284);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(105, 26);
+            this.textBox4.TabIndex = 41;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(552, 287);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 20);
+            this.label10.TabIndex = 40;
+            this.label10.Text = "Phone";
+            // 
+            // buttonOptional
+            // 
+            this.buttonOptional.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.buttonOptional.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.buttonOptional.Location = new System.Drawing.Point(809, 223);
+            this.buttonOptional.Name = "buttonOptional";
+            this.buttonOptional.Size = new System.Drawing.Size(90, 28);
+            this.buttonOptional.TabIndex = 42;
+            this.buttonOptional.Text = "Optional";
+            this.buttonOptional.UseVisualStyleBackColor = false;
+            // 
+            // comboBoxMobileInfo
+            // 
+            this.comboBoxMobileInfo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBoxMobileInfo.FormattingEnabled = true;
+            this.comboBoxMobileInfo.Items.AddRange(new object[] {
+            "Mobile",
+            "Home Landline"});
+            this.comboBoxMobileInfo.Location = new System.Drawing.Point(809, 284);
+            this.comboBoxMobileInfo.Name = "comboBoxMobileInfo";
+            this.comboBoxMobileInfo.Size = new System.Drawing.Size(60, 21);
+            this.comboBoxMobileInfo.TabIndex = 43;
+            this.comboBoxMobileInfo.Text = "Mobile";
+            // 
             // Information
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1150, 771);
+            this.Controls.Add(this.comboBoxMobileInfo);
+            this.Controls.Add(this.buttonOptional);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBoxState);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labelContactInfo);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxAddressInfo);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelSequrityAns);
+            this.Controls.Add(this.comboBoxSequrityQues);
+            this.Controls.Add(this.labelSequrityQues);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.labelRetyperPassword);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBoxPasswordReg);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelPasswordInfo);
             this.Controls.Add(this.textBoxRetypedEmail);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.labelEmailHint);
@@ -390,9 +617,27 @@ namespace Lab_Performance
         private System.Windows.Forms.TextBox textBoxRetypedEmail;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBoxPasswordReg;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelPasswordInfo;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label labelRetyperPassword;
+        private System.Windows.Forms.Label labelSequrityQues;
+        private System.Windows.Forms.ComboBox comboBoxSequrityQues;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelSequrityAns;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxState;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelContactInfo;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxAddressInfo;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button buttonOptional;
+        private System.Windows.Forms.ComboBox comboBoxMobileInfo;
     }
 }
